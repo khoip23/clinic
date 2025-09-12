f04e7a2ad65e5babad6623f06fb9600596ab2a4a
khoi <khoimap2307@gmail.com>
2025-09-12 15:55:04 +0700

    CM-17-Create-Enpoint-RegisterUser

---
+++ Clinic.Application/DTOs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Application
+{
+    public class DTOs
+    {
+        public string UserName { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string FullName { get; set; } = "";
+        public DateTime DOB { get; set; }
+    }
+}
