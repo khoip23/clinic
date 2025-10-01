7cae2d99c645afab95dc6736742e18308d6e8844
khoi <khoimap2307@gmail.com>
2025-10-01 10:10:40 +0700

    CM-28-Migrations

---
+++ Clinic.Application/DTOs/CreateAppointmentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Application.DTOs
+{
+    public class CreateAppointmentRequest
+    {
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public int? DoctorId { get; set; }
+    }
+}
--- Clinic.Infrastructure/Migrations/20251001030654_CM-28-Add-PatientRecord-Table-and-Patient-Table.Designer.cs
+++ Clinic.Infrastructure/Migrations/20251001030654_CM-28-Add-PatientRecord-Table-and-Patient-Table.Designer.cs
@@ -4,6 +4,7 @@ using Clinic.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Clinic.Infrastructure.Migrations
 {
     [DbContext(typeof(ClinicDbContext))]
-    partial class ClinicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251001030654_CM-28-Add-PatientRecord-Table-and-Patient-Table")]
+    partial class CM28AddPatientRecordTableandPatientTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
