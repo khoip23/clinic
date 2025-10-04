cdfed07342cc2211fcecad593b59831a4c708ebf
khoipc <khoimap2307@email.com>
2025-10-04 20:40:06 +0700

    Setup blazor app

---
+++ Clinic.Blazor/wwwroot/app.js
@@ -0,0 +1,9 @@
+﻿function setToken(key, value) {
+    localStorage.setItem(key, value);
+}
+function getToken(key) {
+    return localStorage.getItem(key);
+}
+function removeToken(key) {
+    localStorage.removeItem(key);
+}
