1910869f77c5d83ca59f603d826b50274fb2cdc2
khoi <khoimap2307@gmail.com>
2025-11-20 14:31:41 +0700

    CM-14-FE-UI-FORM-ĐĂNG-KÝ-KHÁM

---
+++ Clinic.Blazor/DTOs/CreateAppointmentRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Clinic.Blazor.DTOs
+{
+    public class CreateAppointmentRequestDto
+    {
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public int? DoctorId { get; set; }
+    }
+}
