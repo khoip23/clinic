44367a946c30f90021752bff12900ce129789e73
khoi <khoimap2307@gmail.com>
2025-11-22 13:20:24 +0700

    CM-21-LOADING-APPOINTMENT-LIST

---
+++ Clinic.Blazor/DTOs/AppointmentPatientDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Clinic.Blazor.DTOs
+{
+    public enum StatusAppointment
+    {
+        Active = 1,
+        Visitted = 2,
+        Canceled = 3,
+    }
+    public class AppointmentPatientDto
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public int Status { get; set; }
+        public int? DoctorId { get; set; }
+    }
+}
